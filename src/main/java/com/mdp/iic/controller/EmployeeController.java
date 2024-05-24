66e0e32f3d2375d411eb1bf4ad3421434a611b7e
Kundan.kumar <Kundan.kumar@iinterchange.com>
2024-05-24 18:21:55 +0530

    Complete Rest Crud API

--- src/main/java/com/mdp/iic/controller/EmployeeController.java
+++ src/main/java/com/mdp/iic/controller/EmployeeController.java
@@ -3,10 +3,16 @@ package com.mdp.iic.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mdp.iic.exception.ResourceNotFoundException;
 import com.mdp.iic.model.Employee;
 import com.mdp.iic.repository.EmployeeRepo;
 
@@ -17,8 +23,39 @@ public class EmployeeController {
 	@Autowired
 	private EmployeeRepo employeeRepo;
 	
-	@GetMapping("allEmployees")
+	@GetMapping("/allEmployees")
 	public List<Employee> 	getAllEmployees(){
 		return employeeRepo.findAll();
 	}
+	
+	//create employee rest api 
+	@PostMapping("/addEmployees")
+	public Employee createEmployee(@RequestBody Employee employee) {
+		return employeeRepo.save(employee);
+	}
+	
+	//get the employee by id rest api
+	@GetMapping("/employee/{id}")
+	public  ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
+		Employee employee =  employeeRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee Not Exist" + id));
+		return ResponseEntity.ok(employee);
+	}
+	
+	//Update the data 
+	@PutMapping("/employee/{id}")
+	public  ResponseEntity<Employee> updateEmployee(@PathVariable Long id, @RequestBody Employee employeeDetails) {
+		Employee employee =  employeeRepo.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee Not Exist" + id));
+		
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmail(employeeDetails.getEmail());
+		
+		Employee updateEmployee = employeeRepo.save(employee);
+		return ResponseEntity.ok(updateEmployee);
+		
+		
+	}
+	
+	
+	
 }
